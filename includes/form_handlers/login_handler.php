1a4af1b8e306ad8e34b31308d49ecfb5b9249800
Vishal Sharma <vsunite3@gmail.com>
2021-03-21 23:05:43 +0530

    Fixed Login Issue

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -3,10 +3,8 @@
 if(isset($_POST['signInButton'])) {
 
 	$email = filter_var($_POST['loginEmail'], FILTER_SANITIZE_EMAIL); //sanitize email
-
-	$_SESSION['loginEmail'] = $email; //Store email into session variable 
+ 
 	$password = md5($_POST['loginPassword']); //Get password
-
 	$check_database_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND password='$password'");
 	$check_login_query = mysqli_num_rows($check_database_query);
 
@@ -17,6 +15,7 @@ if(isset($_POST['signInButton'])) {
 		if(mysqli_num_rows($user_closed_query) == 1) {
 			$reopen_account = mysqli_query($con, "UPDATE users SET user_closed='no' WHERE email='$email'");
 		}
+		$_SESSION['loginEmail'] = $email; //Store email into session variable
 		header("Location: index.php");
 		exit();
 	}
